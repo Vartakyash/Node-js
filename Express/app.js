3004f9bfce02d89cd9cc4394e996fc07ff6838e7
Vartakyash <yashvartak198@gmail.com>
2024-08-27 09:33:35 +0000

    app.js

--- Express/app.js
+++ Express/app.js
@@ -1,1 +1,27 @@
-console.log(`Server is running on http://localhost:${port}`);
+//npm init to be used after cd Express
+//npm install express after cd Express
+
+const express = require('express')
+
+const app = express();
+port = 5000;
+
+app.get(('/'),(req,res)=>{
+    res.send('Home Page')
+})
+
+app.get(('/about'),(req,res)=>{
+    res.send('About Page')
+})
+
+app.get(('/contact'),(req,res)=>{
+    res.status(404).send('Contact Page')
+})
+
+app.get(('/contact'),(req,res)=>{
+    res.status(404).send('Contact Page PostApi')
+})
+
+app.listen(port,()=>{
+    console.log(Server is running on port ${port})
+})
