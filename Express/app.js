90fbf71fcc81e80fd28381d0cc09e5670d3b8d4f
Vartakyash <yashvartak198@gmail.com>
2024-08-27 08:50:24 +0000

    app.js

---
+++ Express/app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+
+const app = express();
+port = 80;
+
+app.get(('/'),(req,res)=>{
+    res.send('Home Page')
+})
+
+
+app.get(('/about'),(req,res)=>{
+    res.send('About Page')
+})
+
+app.get(('/contact'),(req,res)=>{
+    res.status(404).send('Contact Page')
+})
+
+app.get(('/contact'),(req,res)=>{
+    res.status(404).send('Contact Page PostApi')
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)
+})
