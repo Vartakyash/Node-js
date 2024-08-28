8f5ec3035d6db9a909e88cc871c085014ec5bf49
Vartakyash <yashvartak198@gmail.com>
2024-08-28 17:50:01 +0000

    app.js

--- Static-Express/app.js
+++ Static-Express/app.js
@@ -4,3 +4,17 @@ const app = express();
 port = 80;
 
 //For serving static Files
+app.use('/static',express.static('static'))
+
+app.set('view engine','pug');// set the template engine as pug
+
+app.set('views',path.join(__dirname,'views'));// set the view directory
+
+//our pug demo endpoint
+app.get('/demo',(req,res)=>{
+    res.status(200).render('demo', {title: 'First Pug Page',message:'Pug Page',mq:'Breaking News'})
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)
+})
