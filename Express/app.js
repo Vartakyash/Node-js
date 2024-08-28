fd06ca7a43b9de9d367c7b164b49d8c79276c1c9
Vartakyash <yashvartak198@gmail.com>
2024-08-28 08:51:32 +0000

    app.js

--- Express/app.js
+++ Express/app.js
@@ -3,7 +3,17 @@
 // npm i nodemon after cd Express
 //npm install -g nodemon
 //Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
-//nodemon .\app.js
+//nodemon app.js
+//post man will only run when we have done nodemon app.js after cd Express
+//After terminal says Server is running on port 80 open browser type localhost:80
+//We can type localhost/about or localhost/contact 
+//*************************************************//
+//In postman we can type localhost/contact but we will get 'Contact Page PostApi' in console when we use POST
+//instead of GET
+// because we are using app.post
+//app.post(('/contact'),(req,res)=>{
+//     res.status(404).send('Contact Page PostApi')
+// })
 
 
 const express = require('express')
